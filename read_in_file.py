546ecdf9207653bbb54ef7f12c3bd3f0203b67e3
precieuxfassinou <precieuxfassinou@gmail.com>
2026-07-16 22:26:41 +0000

    read in file + line count + input method to read the file

--- read_in_file.py
+++ read_in_file.py
@@ -1,6 +1,8 @@
-xfile= open('mbox.txt')
+inputf = input('Enter the file name: ')
+xfile= open(inputf)
 count = 0
 for cheese in xfile:
         count = count + 1
+        cheese = cheese.rstrip()
         print(cheese)
-print('Line count:', count)
+print('\nLine count:', count)
